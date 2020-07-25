d49a8e9c30d62588bb1f2849dbbdb4d188cddcd3
Maxstupo <maxstupo@hotmail.com>
2020-07-25 21:02:55 +1000

    Initial commit.

---
+++ DynWallpaper/Forms/FormMain.cs
@@ -0,0 +1,21 @@
+﻿namespace Maxstupo.DynWallpaper.Forms {
+    
+    using System;
+    using System.Windows.Forms;
+
+    public partial class FormMain : Form {
+
+        public FormMain() {
+            InitializeComponent();
+
+        }
+
+        private void FormMain_Load(object sender, EventArgs e) {
+
+
+
+        }
+
+    }
+
+}
---
+++ DynWallpaper/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Maxstupo.DynWallpaper {
+
+    using System;
+    using System.Windows.Forms;
+    using Maxstupo.DynWallpaper.Forms;
+
+    static class Program {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+
+        }
+
+    }
+
+}
