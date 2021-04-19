6c099de200799e3f6beacb25a8aa3d75a7b22b32
Maxstupo <maxstupo@hotmail.com>
2021-04-20 03:51:52 +1000

    allow only one process

--- DynamicPaper/Program.cs
+++ DynamicPaper/Program.cs
@@ -1,19 +1,81 @@
 ﻿namespace Maxstupo.DynamicPaper {
 
     using System;
+    using System.IO;
+    using System.IO.Pipes;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+    using System.Security.AccessControl;
+    using System.Security.Principal;
+    using System.Text;
+    using System.Threading;
     using System.Windows.Forms;
     using Maxstupo.DynamicPaper.Forms;
 
-    static class Program {
+    public static class Program {
+        public const string PipeName = "DynamicPaper_IO";
 
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() {
+        static void Main(string[] args) {
+
+            // Get the application GUID defined in AssemblyInfo.cs
+            string guid = ((GuidAttribute) Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
+
+            // id for global mutex (global to machine)
+            string mutexId = string.Format("Global\\{{{0}}}", guid);
+
+            MutexAccessRule allowEveryoneRule = new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
+            MutexSecurity securitySettings = new MutexSecurity();
+            securitySettings.AddAccessRule(allowEveryoneRule);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+
+
+            using (Mutex mutex = new Mutex(false, mutexId, out _, securitySettings)) {
+                bool hasHandle = false;
+                try {
+                    try {
+                        hasHandle = mutex.WaitOne(250, false); // Timeout after 250ms.
+                        if (!hasHandle) { // Application is already running.
+                            //  MessageBox.Show("The application is already running.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            SendMessage("bringtofront");
+
+                            Environment.Exit(0);
+                            return;
+                        }
+                    } catch (AbandonedMutexException) {
+                        hasHandle = true; // The mutex was abandoned in another process, it will still get acquired.
+                    }
+
+                    // Start application normally.
+                    Application.Run(new FormMain());
+                } finally {
+                    if (hasHandle)
+                        mutex.ReleaseMutex();
+                }
+            }
+
+        }
+
+        // Connect to the currently running DynamicPaper process and notify it to "bring-to-front"
+        private static void SendMessage(string msg) {
+            try {
+                using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", PipeName, PipeDirection.Out, PipeOptions.None, TokenImpersonationLevel.Identification)) {
+                    pipeClient.Connect(1000);
+                    using (StreamWriter sw = new StreamWriter(pipeClient, new UTF8Encoding(false))) {
+
+                        sw.WriteLine(msg);
+                        sw.Flush();
+                        pipeClient.WaitForPipeDrain();
+                    }
+                }
+            } catch (Exception) {
+                // Catch exception - Not really important, if it fails nothing happens....
+            }
         }
 
     }
